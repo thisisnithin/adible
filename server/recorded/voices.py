7e940c9f838fd889f750a0b9828e55c91e3ef792
1nF0rmed <prkinformed@gmail.com>
2025-02-23 04:20:45 +0530

    update: use Archie

--- server/recorded/voices.py
+++ server/recorded/voices.py
@@ -8,7 +8,7 @@ class Voice(BaseModel):
     language: str
 
 AVAILABLE_VOICES = [
-    Voice(id="vBKc2FfBKJfcZNyEt1n6", description="A well-connected, young conversational male that's perfect for podcasting or casual conversations.", language="English"),
+    Voice(id="WTUK291rZZ9CLPCiFTfh", description="A well-connected, young conversational male that's perfect for podcasting or casual conversations.", language="English"),
     Voice(id="mhgBlD8CmCSdwLDOIJpA", description="Sharp, modern, conversational, and fast-paced, viral-ready news updates with urgency and bite", language="English"),
     Voice(id="7H4vk4UjjbKDZiGTgH8K", description="A smooth yet assertive middle aged female voice great for a variety of projects including news, social media, conversational, audiobooks, and instructional videos.", language="English"),
     Voice(id="IvLWq57RKibBrqZGpQrC", description="An energetic Hindi lively voice with standard accent. The voice brimming with energy,", language="Hindi"),
