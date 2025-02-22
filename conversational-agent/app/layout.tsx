c47fb883737a3707f5a5d7ee2a434b0d992883d0
thisisnithin <nithinkumar5353@gmail.com>
2025-02-22 23:10:32 +0530

    update logo

--- conversational-agent/app/layout.tsx
+++ conversational-agent/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { BackgroundWave } from "@/components/background-wave";
 import Link from "next/link";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Adible Agent",
@@ -19,7 +20,15 @@ export default function RootLayout({
               "sm:fixed w-full top-0 left-0 grid grid-cols-2 py-4 px-8"
             }
           >
-            <div className="text-xl font-bold">Adible</div>
+            <h1 className="flex items-center gap-x-2 text-3xl font-black mb-8 -ml-2">
+              <Image
+                src="/headphone.png"
+                width={64}
+                height={64}
+                alt="Adible logo"
+              />
+              Adible
+            </h1>
           </nav>
           {children}
           <BackgroundWave />
